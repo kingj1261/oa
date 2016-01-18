30c0f52e0c59cf3394466ef2cfb2b2f93998ba71
Sharpe <maosp@shishike.com>
2016-01-18 13:28:01 +0800

    Add：督办提醒消息业务代码

---
+++ oa-message/src/main/java/com/wantai/oa/remind/Remind.java
@@ -0,0 +1,7 @@
+package com.wantai.oa.remind;
+
+/**
+ * @author Sharpe on 2016-01-16.
+ */
+public interface Remind {
+}
---
+++ oa-message/src/main/java/com/wantai/oa/supervise/Supervise.java
@@ -0,0 +1,9 @@
+package com.wantai.oa.supervise;
+
+/**
+ * @author Sharpe on 2016-01-16.
+ */
+public class Supervise {
+
+
+}
